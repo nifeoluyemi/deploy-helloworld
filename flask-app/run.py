e3c706cb591fdbb5019feb801b42c9d0b12d6874
Nife Oluyemi <nifemioluyemi@yahoo.com>
2023-07-20 10:50:31 -0400

    get flask app running

--- flask-app/run.py
+++ flask-app/run.py
@@ -1,11 +1,17 @@
 from flask import Flask
+import os
 
 app = Flask(__name__, template_folder='template')
 
 @app.route('/')
-def hello():
+def index():
     return 'Hello, World!'
 
 @app.route('/health')
-def hello():
+def health():
     return '', 200
+
+port = int(os.getenv('FLASK_PORT', 5000))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port, debug=True)
